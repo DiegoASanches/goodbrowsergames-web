1fb934c05669aeb29fadb0e369982a350218a1dd
salles-g <salles.gustavog@gmail.com>
2022-06-04 23:35:09 -0300

    feat(Game): get info from API

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-game-component',
@@ -8,16 +9,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GameComponent implements OnInit {
 
-  @Input() title: string = "FEZ";
-  @Input() yourRating: string = "";
-  @Input() gbgRating: string = "5.0";
-  @Input() cover: string = "https://assets1.ignimgs.com/thumbs/userUploaded/2015/1/15/fez-title-1421351820022.jpg";
-  @Input() profilePicture: string = "https://i.imgur.com/ZsJTb3r.png";
-  @Input() description: string = "Ex sunt aliquip deserunt aliquip proident. Commodo irure ad anim dolor non ex adipisicing dolor commodo proident irure.";
+  title: string = "";
+  cover: string = "https://i.imgur.com/2VcvwBZ.png";
+  yourRating: string = "";
+  description: string = "";
+  gbgRating: string = "5.0";
+  profilePicture: string = "https://i.imgur.com/ZsJTb3r.png";
 
   gameId: string;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private service: GameService) {
     // get the :id from the route
     this.route.params.subscribe(params => {
       this.gameId = params.id;
@@ -25,6 +26,12 @@ export class GameComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.service.getOneGame(this.gameId).subscribe(data => {
+      this.title = data.title;
+      this.gbgRating = String(data.voteCount);
+      this.cover = data.posterPath || this.cover;
+      this.description = data.overview;
+    });
   }
 
 }
