62d51af7d9ddd05f53cdae3562c5cf3151cdf9dc
salles-g <salles.gustavog@gmail.com>
2022-06-05 02:04:13 -0300

    feat: list all user reviews

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import Swal from 'sweetalert2';
-import { EvaluationService } from '../services/evaluation.service';
+import { Evaluation, EvaluationService } from '../services/evaluation.service';
 import { GameService } from '../services/game.service';
 
 @Component({
@@ -24,6 +24,8 @@ export class GameComponent implements OnInit {
   evaluation: string;
   grade: number;
 
+  evaluations: Evaluation[] = [];
+
   form: FormGroup;
 
   constructor(
@@ -41,6 +43,7 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     this.evaluationService.get(this.gameId).subscribe(data => {
       console.log('evaluation', data);
+      this.evaluations = data.results;
     });
     
 
